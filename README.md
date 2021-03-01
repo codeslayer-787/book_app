c5e56a54a9ecf69ec621780f1f64cfd031d49c27
codeslayer-787 <irwin_ayala@yahoo.com>
2021-03-01 17:57:39 -0600

    Adds initial files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# book_app
-A simple app that uses the Google Books API
+## City Explorer
+Author: Irwin Ayala Version: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+### Overview
+This app contains a textbox and radio buttons so that the user can search a book by it's title or author.  It will return a result from Google Books. 
+
+### Getting Started
+Find the app at https://github.com/codeslayer-787/book_app
+
+### Architecture
+This app was writen with HTML5, CSS, and JavaScript. Node.js was used to deploy a virtual server and Heroku for the final deployemnt.
+
+Number and name of feature: Initial Setup
+
+Estimate of time needed to complete: 4 hours
+
+Start time: 3:20pm CST
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+Change Log
+03-01-2021 5:30pm - Uploaded the server files and deployed server.
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+require('dotenv').config();
+
+// const PARKS_API_KEY = process.env.PARKS_API_KEY;
+
+
+const app = express();
+app.use(cors());
+
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/health', handleHealthRequest);
+function handleHealthRequest(req, res) {
+  console.log(handleHealthRequest);
+  const ejsObject = { teacher: 'nicholas', course: '301d71', funFactor: 7 };
+  res.render('pages/index.ejs', ejsObject);
+}
+
+app.get('/books', handleGetBooks);
+function handleGetBooks(req, res) {
+  superagent.get(`https://www.googleapis.com/books/v1/volumes?q=search+inauthor`)
+    .then(booksData => {
+      console.log(booksData.body.items);
+      const booksArray = booksData.body.items.map(newBook => new Book(newBook));
+      res.render('pages/index.ejs', booksArray);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('Error loading books data');
+    });
+}
+function Book(object) {
+  this.title = object.title;
+  this.author = object.authors;
+}
+
+
+app.listen(PORT, () => console.log(`app is alive ${PORT}`));
