332aa7fd310a3315ec9eff59e70b0722ab27a3f6
codeslayer-787 <irwin_ayala@yahoo.com>
2021-03-03 22:14:04 -0600

    Adds bug fixes

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const cors = require('cors');
 const superagent = require('superagent');
 require('dotenv').config();
 const pg = require('pg');
-// const { res } = require('express');
-
-
-// const PARKS_API_KEY = process.env.PARKS_API_KEY;
-
+const methodOverride = require('method-override');
 
 const app = express();
 app.use(cors());
@@ -22,6 +18,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs'); //Changes the setting of how we render html to use ejs.
+app.use(methodOverride('_method'));
 
 // Sets Index.ejs as our homepage
 app.get('/', (req, res) => {
@@ -29,22 +26,23 @@ app.get('/', (req, res) => {
   client.query(sql)
     .then(result => {
       let allBooks = result.rows;
+      // console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$', allBooks);
       res.render('./pages/index.ejs', { bookList: allBooks });
     });
 });
 
-app.get('/books', renderSearchPage);
-app.post('/search', handleGetBooks);
+app.get('/search', renderSearchResults);
+app.post('/searches', handleGetBooks);
 app.get('/books/:id', singleBookDetails);
-
+app.post('/books', savedBooks);
+app.put('/books/:id', updateBooks);
+app.delete('/books/:id', deleteBooks);
 app.get(`*`, handleError);
-function renderSearchPage(req, res) {
-  res.render('pages/searches/new.ejs');
-}
+
 function handleGetBooks(req, res) {
   const searchQuery = req.body.input;
   const searchType = req.body.type;
-  console.log('this is the body', req.body);
+  // console.log('this is the body', req.body);
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
   if (searchType === 'title') {
     url += `intitle=${searchQuery}`;
@@ -52,12 +50,11 @@ function handleGetBooks(req, res) {
   if (searchType === 'author') {
     url += `inauthor=${searchQuery}`;
   }
-  console.log(url);
   superagent.get(url)
     .then(booksData => {
       const bookArray = booksData.body.items;
       const finalBooksArray = bookArray.map(newBook => new Book(newBook.volumeInfo));
-      console.log(bookArray + 'book array here');
+      console.log(' ????????????????????????????????????????????????????????????????????????', finalBooksArray);
       res.render('pages/searches/show.ejs', { finalBooksArray: finalBooksArray });
     })
     .catch(error => {
@@ -66,32 +63,60 @@ function handleGetBooks(req, res) {
     });
 }
 
+function renderSearchResults(req, res) {
+  res.render('pages/searches/new.ejs');
+}
+
 function handleError(error, res) {
-  console.log(error);
-  res.render('pages/error', { error: error });
+  // console.log(error);
+  res.render('pages/error.ejs', { error: error });
 
 }
 function singleBookDetails(req, res) {
   const id = req.params.id;
-  console.log('single book details: ', id);
+  // console.log('single book details: ', id);
   const sql = 'SELECT * FROM books WHERE id=$1;';
   const storedValues = [id];
   client.query(sql, storedValues).then((results) => {
-    console.log(results.rows[0]);
     const chosenOne = results.rows[0];
     res.render('pages/books/detail', { chosenOne: chosenOne });
   });
 }
+function savedBooks(req, res) {
+  const { author, title, isbn, image_url, description } = req.body;
+  // console.log('Anything HERE', req.body);
+  const sql = 'INSERT INTO books (author, title, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5) RETURNING *;';
+  const storedValues = [author, title, isbn, image_url, description];
+  client.query(sql, storedValues)
+    .then(results => {
+      const id = results.rows[0].id;
+      // console.log('This data from savedBooks function', results);
+      res.redirect(`books/${results.rows[0].id}`);
+    });
+}
+function updateBooks(req, res) {
+  const id = req.params.id;
+  const {author, title, isbn, image_url, description} = req.body;
+  let sql = 'UPDATE books SET author=$1, title=$2, isbn=$3, image_url=$4, description=$5 WHERE id=$6';
+  let storedValues = [author, title, isbn, image_url, description, id];
+  client.query(sql, storedValues);
+  res.status(200).redirect(`/books${id}`);
+}
 
-
+function deleteBooks(req, res) {
+  const id = req.params.id;
+  let sql = 'DELETE FROM books WHERE id=$1';
+  let storedValues = [id];
+  client.query(sql, storedValues);
+  res.status(200).redirect('../');
+}
 
 function Book(object) {
   this.title = object.title;
   this.author = object.authors;
   this.description = object.description;
-  this.image = object.imageLinks.thumbnail;
+  this.image = object.imageLinks.thumbnail || 'no image found';
   this.isbn = object.industryIdentifiers[0].identifier;
-  console.log(this.isbn, 'need isbn');
 }
 
 client.connect().then(() => {
